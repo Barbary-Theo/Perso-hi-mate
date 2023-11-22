e706b5971534f45501100673cfe6f48ae8353117
Barbary-Theo <theo.barbary25@gmail.com>
2023-11-22 10:53:45 +0100

    sus add proxy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  devServer: {
+    allowedHosts: 'all',
+    host: 'barbary-theo.com',
+    proxy: {
+      // Proxy all requests to your local server
+      '/': {
+        target: 'http://localhost:8080', // Change this to your local server's port
+        bypass: function(req, res) {
+          // Allow requests from any origin
+          res.setHeader('Access-Control-Allow-Origin', '*');
+        }
+      }
+    }
+  }
+}
